bd3a28555246cefe3b7ab14bd9a57952de412607
Graeme Byrne <grmbrn89@gmail.com>
2025-04-11 17:55:28 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-04-11',
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    defaults: {
+      useAsyncData: {
+        deep: true
+      }
+    }
+  },
+
+  image: {
+    domains: ['https://cdn.dummyjson.com']
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    }
+  },
+
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+
+  googleFonts: {
+    families: {
+      Montserrat: true
+    }
+  }
+})
